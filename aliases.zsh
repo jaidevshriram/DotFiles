245352840b893e567a8e5d0307214b1ecbdbe59c
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-12-07 23:04:17 +0530

    Added aliased used in current ZSH setup - Deepin

---
+++ aliases.zsh
@@ -0,0 +1,6 @@
+alias ga="git add ."
+alias gcm="git commit -m $1"
+alias gp="git push"
+alias gts="git status"
+alias gstd="git add .; git commit -m \"Minor Update, Standard Message\"; git push"
+alias desktop="cd ~/Desktop"
