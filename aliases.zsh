0b83949f1c1b446bec75ba23b6a358853f01563e
Jaidev Shriram <jaidevtrumpet@gmail.com>
2020-05-19 18:51:34 +0530

    Added explorer open as alias

--- aliases.zsh
+++ aliases.zsh
@@ -3,10 +3,11 @@ alias gcm="git commit -m $1"
 alias gp="git push"
 alias gts="git status"
 alias gstd="git add .; git commit -m \"Minor Update, Standard Message\"; git push"
-alias desktop="cd ~/Desktop"
+alias desktop="/mnt/c/Users/jaide/Desktop"
 
 alias rm="rm -i"
 
 source /opt/ros/melodic/setup.zsh
 export DISPLAY=:0
 alias py3="python3"
+alias open="explorer.exe ."
